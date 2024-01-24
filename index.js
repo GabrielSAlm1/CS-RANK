6db052376bc6878cd56cc56ed6b2d3d4b81da235
Gabriel S Almeida <gabriel10santos2012@gmail.com>
2024-01-24 20:44:07 -0300

    pporta dnv

--- index.js
+++ index.js
@@ -49,20 +49,15 @@ async function processarSteamIds() {
   for (const steamId of steamIds) {
     await robo(steamId);
   }
-
-  // Salvar os resultados em um arquivo JSON
-  const jsonResultados = JSON.stringify(resultados, null, 2);
-  await fs.writeFile('resultado.json', jsonResultados);
 }
 
 const app = express();
 
 app.get('/resultado', async (req, res) => {
   await processarSteamIds();
-
-  // Ler o arquivo e enviar como resposta
-  const fileContents = await fs.readFile('resultado.json', 'utf-8');
-  res.json(JSON.parse(fileContents));
+  
+  // Retorna os resultados em JSON como resposta à requisição GET
+  res.json(resultados);
 });
 
 // Inicia o servidor Express na porta 3000 (ou na porta definida pela variável de ambiente PORT)
